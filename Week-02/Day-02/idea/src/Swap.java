88c5d7bdefc234592805cab4d3e14e82935ea6e8
Balazs Stegmar <stegmarb@gmail.com>
2017-03-21 15:56:06 +0100

    stucked at exercise 13

---
+++ Week-02/Day-02/idea/src/Swap.java
@@ -0,0 +1,15 @@
+/**
+ * Created by stegmarb on 2017. 03. 21..
+ */
+public class Swap {
+  public static void main(String[] args) {
+    int a = 123;
+    int b = 526;
+    int interNumber = a;
+    a = b;
+    b = interNumber;
+
+    System.out.println(a);
+    System.out.println(b);
+  }
+}
