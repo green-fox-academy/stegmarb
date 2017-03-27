42a8d75f8e0610b0a6a2c6f8db0672f59f479c80
Balazs Stegmar <stegmarb@gmail.com>
2017-03-27 13:09:57 +0200

    numbers are sorted from lowest to highest

--- Week-03/Day-01/src/Lotto.java
+++ Week-03/Day-01/src/Lotto.java
@@ -2,10 +2,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * Created by stegmarb on 2017. 03. 27..
@@ -20,12 +17,18 @@ public class Lotto {
       for ( String line : lines) {
         String stringLines = line;
         String[] separated = stringLines.split(";");
-        for (int i = separated.length-1; i > separated.length-5; i--) {
+        for (int i = separated.length-1; i > separated.length-6; i--) {
             numbers.add(separated[i]);
         }
       }
-      
-      System.out.println(numbers);
+      ArrayList<Integer> realNumbers = new ArrayList<>();
+      int[] arrayNumbers;
+      for (String number : numbers) {
+        realNumbers.add(Integer.parseInt(number));
+      }
+      Collections.sort(realNumbers);
+
+      System.out.println(realNumbers);
     } catch (IOException e) {
       System.out.println("Ops! There is something wrong with the file");
     }
