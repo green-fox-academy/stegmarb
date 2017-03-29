3fa18607117a30c286c5da7ee0a2e570d4205601
Balazs Stegmar <stegmarb@gmail.com>
2017-03-29 15:22:47 +0200

    LinePlay is fully functional regardless of the size of canvas

---
+++ Week-03/Day-03/src/LinePlay.java
@@ -0,0 +1,50 @@
+import javax.swing.*;
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+/**
+ * Created by stegmarb on 2017. 03. 29..
+ */
+public class LinePlay {
+  public static int dimensionX = 500;
+  public static int dimensionY = 500;
+
+  public static void mainDraw(Graphics graphics) {
+    parabolas(graphics);
+
+  }
+
+  //    Don't touch the code below
+  public static void main(String[] args) {
+    JFrame jFrame = new JFrame("Drawing");
+    jFrame.setSize(new Dimension(dimensionX, dimensionY));
+    jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    jFrame.add(new ImagePanel());
+    jFrame.setLocationRelativeTo(null);
+    jFrame.setVisible(true);
+  }
+
+  static class ImagePanel extends JPanel {
+    @Override
+    protected void paintComponent(Graphics graphics) {
+      super.paintComponent(graphics);
+      mainDraw(graphics);
+
+    }
+  }
+  public static void parabolas (Graphics g) {
+
+    for (int i = 0; i < dimensionX ; i+= 20) {
+      for (int j = 0; j < dimensionX; j+=20) {
+        if (i == 0) {
+          g.setColor(Color.RED);
+          g.drawLine(j, i, dimensionX, j/20*(dimensionY/(dimensionX/20)));
+        } else if (j == 0) {
+          g.setColor(Color.BLUE);
+          g.drawLine(j, i/20*(dimensionY/(dimensionX/20)), i, dimensionY);
+        }
+      }
+    }
+  }
+}
