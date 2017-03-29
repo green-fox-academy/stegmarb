46ed7147736565df7328b75d5e8ddce57d9a37f6
Balazs Stegmar <stegmarb@gmail.com>
2017-03-29 11:21:21 +0200

    CenteredSquare exercise is ready

---
+++ Week-03/Day-03/src/CenteredSquare.java
@@ -0,0 +1,34 @@
+import javax.swing.*;
+import java.awt.*;
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+/**
+ * Created by stegmarb on 2017. 03. 29..
+ */
+public class CenteredSquare {
+
+  public static void mainDraw(Graphics graphics){
+    // draw a green 10x10 square to the canvas' center.
+    graphics.setColor(Color.GREEN);
+    graphics.drawRect(145, 145, 10, 10);
+
+
+  }
+
+  //    Don't touch the code below
+  public static void main(String[] args) {
+    JFrame jFrame = new JFrame("Drawing");
+    jFrame.setSize(new Dimension(300, 300));
+    jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    jFrame.add(new ImagePanel());
+    jFrame.setLocationRelativeTo(null);
+    jFrame.setVisible(true);
+  }
+  static class ImagePanel extends JPanel{
+    @Override
+    protected void paintComponent(Graphics graphics) {
+      super.paintComponent(graphics);
+      mainDraw(graphics);
+    }
+  }
+}
