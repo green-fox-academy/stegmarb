28fa2145e79907442e0729cb617c8ffbfd69c1e1
Balazs Stegmar <stegmarb@gmail.com>
2017-03-29 10:07:16 +0200

    Diagonals exercise is ready

--- Week-03/Day-03/src/Diagonals.java
+++ Week-03/Day-03/src/Diagonals.java
@@ -2,22 +2,20 @@ import javax.swing.*;
 import java.awt.*;
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
-
 /**
  * Created by stegmarb on 2017. 03. 29..
  */
-public class LineInTheMiddle {
-
+public class Diagonals {
   public static void mainDraw(Graphics graphics){
 
-    graphics.setColor(Color.GREEN);
-    graphics.drawLine(0, 150, 300, 150);
+  graphics.setColor(Color.GREEN);
+  graphics.drawLine(0,0,300,300);
 
-    graphics.setColor(Color.RED);
-    graphics.drawLine(150, 0, 150, 300);
+  graphics.setColor(Color.GREEN);
+  graphics.drawLine(0,300,300,0);
+
 
   }
-
 
   //    Don't touch the code below
   public static void main(String[] args) {
@@ -28,7 +26,8 @@ public class LineInTheMiddle {
     jFrame.setLocationRelativeTo(null);
     jFrame.setVisible(true);
   }
-  static class ImagePanel extends JPanel {
+  static class ImagePanel extends JPanel{
+    @Override
     protected void paintComponent(Graphics graphics) {
       super.paintComponent(graphics);
       mainDraw(graphics);
