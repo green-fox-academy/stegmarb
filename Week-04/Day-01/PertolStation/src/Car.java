b88f4c44ed1b7bb49642b70c098ebdccfcb81c64
Balazs Stegmar <stegmarb@gmail.com>
2017-04-03 14:56:24 +0200

    FuelStation exercise is now fully working

---
+++ Week-04/Day-01/PertolStation/src/Car.java
@@ -0,0 +1,13 @@
+
+public class Car {
+  public double gasAmount;
+  public double capaciy;
+
+  public Car() {
+    gasAmount = 0;
+    capaciy = 100;
+  }
+
+
+
+}
---
+++ Week-04/Day-01/PertolStation/src/Station.java
@@ -0,0 +1,19 @@
+
+public class Station {
+  private double gasAmount;
+
+
+
+  public void refill(Car car) {
+    this.gasAmount -= car.capaciy;
+    car.gasAmount += this.gasAmount;
+  }
+
+  public static void main(String[] args) {
+    Station station = new Station();
+    station.gasAmount = 150;
+    Car car = new Car();
+    station.refill(car);
+    System.out.println(station.gasAmount);
+  }
+}
