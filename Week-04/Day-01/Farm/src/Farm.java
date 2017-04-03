48acb7c55ec8f5a5aba43b8c582f275bae43540d
Balazs Stegmar <stegmarb@gmail.com>
2017-04-03 16:52:32 +0200

    Farm is now has all functions, including slaughter and breed methods

--- Week-04/Day-01/Farm/src/Farm.java
+++ Week-04/Day-01/Farm/src/Farm.java
@@ -3,11 +3,11 @@ import java.util.List;
 
 public class Farm {
   private List<Animal> animals = new ArrayList();
-  private int slots = 10;
+  private int slots = 3;
 
   public void breed() {
     if (animals.size() < slots) {
-      for (int i = animals.size(); i < (animals.size() - slots)+ animals.size(); i++) {
+      for (int i = 0; i < (slots - animals.size())+ animals.size(); i++) {
         animals.add(new Animal());
       }
     }
@@ -20,6 +20,7 @@ public class Farm {
       if (animals.get(i).getHungerValue() > countHunger) {
         countHunger = animals.get(i).getHungerValue();
         position = i;
+
       }
     }
     animals.remove(position);
@@ -30,10 +31,9 @@ public class Farm {
     littleFarm.animals.add(new Animal(25,15));
     littleFarm.animals.add(new Animal(42,15));
     littleFarm.animals.add(new Animal(15,15));
-    littleFarm.breed();
     littleFarm.slaughter();
     littleFarm.breed();
 
-    System.out.println(littleFarm.animals.get(2).getHungerValue());
+    System.out.println(littleFarm.animals.get(1).getHungerValue());
   }
 }
