59d6ff4524150eece52c48430e1629526552efa4
Balazs Stegmar <stegmarb@gmail.com>
2017-04-03 14:13:44 +0200

    Animals exercise is now ready

---
+++ Week-04/Day-01/Animal/src/Animal.java
@@ -0,0 +1,28 @@
+
+public class Animal {
+  private int hunger;
+  private int thirst;
+
+  public Animal() {
+    hunger = 50;
+    thirst = 50;
+  }
+
+  public void eat() {
+    hunger--;
+  }
+
+  public void drink() {
+    thirst--;
+  }
+
+  public void play() {
+    hunger++;
+    thirst++;
+  }
+
+  public static void main(String[] args) {
+    Animal animal = new Animal();
+    System.out.println(animal.thirst);
+  }
+}
