e2b10659f517f035d9b16471cae19d2f1b3ba9a1
Balazs Stegmar <stegmarb@gmail.com>
2017-04-03 12:03:48 +0200

    Domino ordrer program is now fully functional

---
+++ Week-04/Day-01/src/Dominos.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Dominos {
+  public static void main(String[] args) {
+    List<Domino> dominoes = initializeDominoes();
+    List<Domino> orderedDominoes = new ArrayList<>();
+    orderedDominoes.add(dominoes.get(0));
+    for (int i = 1; i < dominoes.size()-1; i++) {
+      for (int j = 0; j < dominoes.size() ; j++) {
+        if (orderedDominoes.get(i-1).getValues()[1] == dominoes.get(j).getValues()[0]) {
+        orderedDominoes.add(dominoes.get(j));
+        }
+      }
+    }
+    // You have the list of Dominoes
+    // Order them into one snake where the adjacent dominoes have the same numbers on their adjacent sides
+    // eg: [2, 4], [4, 3], [3, 5] ...
+
+    System.out.println(orderedDominoes);
+  }
+
+  static List<Domino> initializeDominoes() {
+    List<Domino> dominoes = new ArrayList<>();
+    dominoes.add(new Domino(5, 2));
+    dominoes.add(new Domino(4, 6));
+    dominoes.add(new Domino(1, 5));
+    dominoes.add(new Domino(6, 7));
+    dominoes.add(new Domino(2, 4));
+    dominoes.add(new Domino(7, 1));
+    return dominoes;
+  }
+}
