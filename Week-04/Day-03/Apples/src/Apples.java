e2742fc02cf4b720b2044f07706f4f69a47f06f8
Balazs Stegmar <stegmarb@gmail.com>
2017-04-05 10:51:07 +0200

    init(apples) - classes and first test is ready

---
+++ Week-04/Day-03/Apples/src/Apples.java
@@ -0,0 +1,11 @@
+public class Apples {
+  private String name;
+
+  public Apples() {
+    name = "apple";
+  }
+
+  public String getName() {
+    return name;
+  }
+}
---
+++ Week-04/Day-03/Apples/test/ApplesTest.java
@@ -0,0 +1,14 @@
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ApplesTest {
+  
+  @Test
+    public void testName() {
+    Apples apple = new Apples();
+    assertEquals("apple", apple.getName());
+  }
+
+}
