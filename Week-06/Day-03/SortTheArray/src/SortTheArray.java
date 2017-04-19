ade3d9988df3c3fbf7be13f8b9608198db123308
Balazs Stegmar <stegmarb@gmail.com>
2017-04-19 16:16:50 +0200

    feat(SortTheAraz): cort any kind of number object array

---
+++ Week-06/Day-03/SortTheArray/src/SortTheArray.java
@@ -0,0 +1,23 @@
+public class SortTheArray {
+  public static void main(String[] args) {
+    Double[] test = {1.2, 2.9, 3.8, 4.45, 5.487};
+    sortMethod(test);
+    System.out.println(test[0]);
+    System.out.println(test[1]);
+    System.out.println(test[2]);
+    System.out.println(test[3]);
+    System.out.println(test[4]);
+  }
+
+  public static <T extends Number> void sortMethod(T[] array) {
+    for (int i = 0; i < array.length; i++) {
+      for (int j = 1; j < array.length; j++) {
+        if (array[j-1].doubleValue() < array[j].doubleValue()) {
+          T temp = array[j-1];
+          array[j-1] = array[j];
+          array[j] = temp;
+        }
+      }
+    }
+  }
+}
