c6b05caf98fe89885ba183aaf2680c74413e7d8c
Balazs Stegmar <stegmarb@gmail.com>
2017-04-20 14:06:41 +0200

    feat(BirthdayCalculator): convert date as string into date format

--- Week-06/Day-04/GradleTest/src/main/java/date/BirthdayWithLocalDate.java
+++ Week-06/Day-04/GradleTest/src/main/java/date/BirthdayWithLocalDate.java
@@ -1,5 +1,6 @@
 package date;
 
+import javax.swing.*;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
@@ -9,27 +10,29 @@ public class BirthdayWithLocalDate implements BirthdayCalculator<LocalDate> {
 
   @Override
   public LocalDate parseDate(String str) {
-    // TODO - return with the parsed date; format is: yyyy-MM-dd
+    String[] dateString = str.split("-");
+    LocalDate date = LocalDate.of(Integer.parseInt(dateString[0]), Integer.parseInt(dateString[1]), Integer.parseInt(dateString[2]));
+    return date;
   }
 
   @Override
   public String printMonthAndDay(LocalDate date) {
-    // TODO - return the date formatted: month & day (MM. dd.)
+//     TODO - return the date formatted: month & day (MM. dd.)
   }
 
   @Override
   public boolean isAnniversaryToday(LocalDate date) {
-    // TODO - return with true if today is the same month+day as date
+//     TODO - return with true if today is the same month+day as date
   }
 
   @Override
   public int calculateAgeInYears(LocalDate birthday) {
-    // TODO - return how many years age the input date 'birthday' was
+//     TODO - return how many years age the input date 'birthday' was
   }
 
   @Override
   public int calculateDaysToNextAnniversary(LocalDate date) {
-    // TODO - the number of days remaining to the next anniversary of 'date' (e.g. if tomorrow, return 1)
+//     TODO - the number of days remaining to the next anniversary of 'date' (e.g. if tomorrow, return 1)
   }
 
   public static void main(String[] args) {
--- Week-06/Day-04/GradleTest/src/test/java/date/TestBirthdayCalculatorWithLocalDate.java
+++ Week-06/Day-04/GradleTest/src/test/java/date/TestBirthdayCalculatorWithLocalDate.java
@@ -1,7 +1,6 @@
 package date;
 
 import org.junit.Test;
-
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
