6ed22c6d4c011f4f5e7e1d7d30a964f43d7744f6
Balazs Stegmar <stegmarb@gmail.com>
2017-04-21 11:25:47 +0200

    feat(TwentyAndOne): player can choose wether want one other card, or not

--- Week-06/Day-03/TwentyPlusOne/src/Game.java
+++ Week-06/Day-03/TwentyPlusOne/src/Game.java
@@ -1,21 +1,21 @@
+import javax.sound.midi.Soundbank;
 import java.util.Scanner;
 
 public class Game {
-  public static Player player = new Player();
-  public static Player house = new Player();
+  public static Player player = new Player("Player");
+  public static Player house = new Player("House");
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
     Deck deck = new Deck();
     deck.fillTheDeck();
     firstDraw(deck);
-    System.out.println("House's hand is:");
-    System.out.println("Hidden");
-    System.out.println(house.getHand().get(1).getRank().toString() + " " + house.getHand().get(1).getSuit() + "\n");
-    System.out.println("Your hand is:");
-    System.out.println(player.getHand().get(0).getRank().toString() + " " + player.getHand().get(0).getSuit());
-    System.out.println(player.getHand().get(1).getRank().toString() + " " + player.getHand().get(1).getSuit());
-    if (scanner.equals("y")) {
-
+    house.printCards();
+    player.printCards();
+    System.out.println("Would you like to have one more card?");
+    String playerResponde = scanner.nextLine();
+    while (!playerResponde.equals("n") && playerResponde.equals("y")) {
+      player.pullOneFromTop(deck);
+      player.printCards();
     }
   }
 
--- Week-06/Day-03/TwentyPlusOne/src/Player.java
+++ Week-06/Day-03/TwentyPlusOne/src/Player.java
@@ -3,6 +3,11 @@ import java.util.List;
 
 public class Player {
   private List<Card> hand = new ArrayList();
+  private String name;
+
+  public Player(String name) {
+    this.name = name;
+  }
 
   public void addCard(Card card) {
     hand.add(card);
@@ -15,5 +20,20 @@ public class Player {
 
   public List<Card> getHand() {
     return hand;
+  }
+
+  public int getBackValue() {
+    int value = 0;
+    for (Card card : hand) {
+     value+=card.getRank().getRankPoints();
+    }
+    return value;
+  }
+
+  public void printCards() {
+    System.out.println("\n" + name + "'s hand is:");
+    for (int i = 0; i < hand.size(); i++) {
+      System.out.println(hand.get(i).getRank() + " " + hand.get(i).getSuit());
+    }
   }
 }
