72b5abd5e3e23932f4df7a1457fe3e6fe29ec4f8
Balazs Stegmar <stegmarb@gmail.com>
2017-04-21 10:52:22 +0200

    feat(TwentyAndOne): print out house's and player's cards

--- Week-06/Day-03/TwentyPlusOne/src/Game.java
+++ Week-06/Day-03/TwentyPlusOne/src/Game.java
@@ -8,10 +8,15 @@ public class Game {
     Deck deck = new Deck();
     deck.fillTheDeck();
     firstDraw(deck);
-    System.out.println(player.getHand().get(0).getRank().getRankPoints() + " " + player.getHand().get(0).getSuit());
-    System.out.println(player.getHand().get(1).getRank().getRankPoints() + " " + player.getHand().get(1).getSuit());
+    System.out.println("House's hand is:");
+    System.out.println("Hidden");
+    System.out.println(house.getHand().get(1).getRank().toString() + " " + house.getHand().get(1).getSuit() + "\n");
+    System.out.println("Your hand is:");
+    System.out.println(player.getHand().get(0).getRank().toString() + " " + player.getHand().get(0).getSuit());
+    System.out.println(player.getHand().get(1).getRank().toString() + " " + player.getHand().get(1).getSuit());
+    if (scanner.equals("y")) {
 
-
+    }
   }
 
   public static void firstDraw(Deck deck) {
