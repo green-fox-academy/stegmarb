1b5fcdfbd1749cbde23bca7022561b1bb5d99204
Balazs Stegmar <stegmarb@gmail.com>
2017-04-27 23:54:50 +0200

    feat(LotteryFilter): has all methods that it should

--- Week-06/Day-04/LotteryFilter/src/main/java/LotteryFilter.java
+++ Week-06/Day-04/LotteryFilter/src/main/java/LotteryFilter.java
@@ -1,14 +1,26 @@
 import au.com.bytecode.opencsv.CSVReader;
+import au.com.bytecode.opencsv.CSVWriter;
+import joptsimple.OptionParser;
+import joptsimple.OptionSet;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class LotteryFilter {
   public static void main(String[] args) {
-    readCsv("D:\\GreenFoxAcademy\\stegmarb\\Week-06\\Day-04\\LotteryFilter\\build\\classes\\main\\otos.csv");
+    OptionParser parser = new OptionParser();
+    parser.accepts("y");
+    parser.accepts("f");
+    parser.accepts("o");
+    OptionSet option = parser.parse(args);
+
+    if (option.has("y")) {
+    }
   }
   public static List<String[]> readCsv(String fileName) {
     try {
@@ -23,7 +35,22 @@ public class LotteryFilter {
     return null;
   }
 
-  public static void writeResults(List<String[]> list) {
-    
+  public static List<String[]> givenLine(List<String[]> list, String number) {
+    List<String[]> temp = new ArrayList<>();
+    for (int i = 0; i < list.size(); i++) {
+      if (list.get(i)[0] == number) {
+        temp.add(list.get(i));
+      }
+    }
+    return temp;
+  }
+
+  public static void writeResults(List<String[]> list, String fileName) {
+    try {
+      CSVWriter writer = new CSVWriter(new FileWriter(fileName), ';', CSVWriter.DEFAULT_QUOTE_CHARACTER);
+      writer.writeAll(list);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
   }
 }
