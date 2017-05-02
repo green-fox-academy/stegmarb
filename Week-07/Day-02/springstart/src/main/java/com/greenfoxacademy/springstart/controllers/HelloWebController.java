a9ef7ade4085dc851b982120b80b869d3dc2cba6
Balazs Stegmar <stegmarb@gmail.com>
2017-05-02 14:35:53 +0200

    feat(springstart): web application can greet the user and count the load count

--- Week-07/Day-02/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
+++ Week-07/Day-02/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
@@ -3,12 +3,17 @@ package com.greenfoxacademy.springstart.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Controller
 public class HelloWebController {
+  AtomicInteger count = new AtomicInteger();
   @RequestMapping("/web/greeting")
-  public String greeting(Model model) {
-    model.addAttribute("name", "World");
+  public String greeting(Model model, @RequestParam(value = "name", defaultValue = "World", required = false) String name) {
+    model.addAttribute("count", count.getAndIncrement());
+    model.addAttribute("name", name);
     return "greeting";
   }
 }
