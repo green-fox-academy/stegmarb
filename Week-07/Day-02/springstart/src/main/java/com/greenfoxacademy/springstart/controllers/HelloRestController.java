adacde0b29a69af6f4ce59926c47178a80c5d548
Balazs Stegmar <stegmarb@gmail.com>
2017-05-02 12:23:44 +0200

    feat(springstart): the url can contain name value and app will greet the user

--- Week-07/Day-02/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRestController.java
+++ Week-07/Day-02/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRestController.java
@@ -2,14 +2,15 @@ package com.greenfoxacademy.springstart.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloRestController {
   @RequestMapping("/greeting")
   @ResponseBody
-  public Greeting greeting() {
-    Greeting greeting = new Greeting(1, "Hello, World!");
+  public Greeting greeting(@RequestParam(value = "name", defaultValue = "Hello, World!", required = false) String name) {
+    Greeting greeting = new Greeting(1, name);
     return greeting;
   }
 }
