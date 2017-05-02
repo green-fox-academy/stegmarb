b12144cb1148b13fcc6799df69570259eabef954
Balazs Stegmar <stegmarb@gmail.com>
2017-05-02 12:34:26 +0200

    feat(springstart): app can count api call number and can show it

--- Week-07/Day-02/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRestController.java
+++ Week-07/Day-02/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRestController.java
@@ -5,12 +5,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 @Controller
 public class HelloRestController {
+  AtomicLong count = new AtomicLong();
   @RequestMapping("/greeting")
   @ResponseBody
   public Greeting greeting(@RequestParam(value = "name", defaultValue = "Hello, World!", required = false) String name) {
-    Greeting greeting = new Greeting(1, name);
+    Greeting greeting = new Greeting(count.getAndIncrement(), name);
     return greeting;
   }
 }
