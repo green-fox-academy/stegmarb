6287620892e9a4fe9af9ac3126009cda563b2d7f
Balazs Stegmar <stegmarb@gmail.com>
2017-05-03 12:00:17 +0200

    feat(lionking): print BankAccount object's fields when /exercise1 is called

---
+++ Week-07/Day-03/LionKing/src/main/java/com/greenfoxacademy/controller/Exercise1.java
@@ -0,0 +1,19 @@
+package com.greenfoxacademy.controller;
+
+
+import com.greenfoxacademy.modell.BankAccount;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Exercise1 {
+  @RequestMapping("/exercise1")
+  public String createBankAccount(Model model) {
+    BankAccount bankAccount = new BankAccount("Simba", "2000", "lion");
+    model.addAttribute("name", bankAccount.getName());
+    model.addAttribute("balance", bankAccount.getBalance());
+    model.addAttribute("animalType", bankAccount.getAnimalType());
+    return "bankaccount";
+  }
+  }
---
+++ Week-07/Day-03/LionKing/src/main/java/com/greenfoxacademy/modell/BankAccount.java
@@ -0,0 +1,25 @@
+package com.greenfoxacademy.modell;
+
+public class BankAccount {
+  private String name;
+  private String balance;
+  private String animalType;
+
+  public BankAccount(String name, String balance, String animalType) {
+    this.name = name;
+    this.balance = balance;
+    this.animalType = animalType;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getBalance() {
+    return balance;
+  }
+
+  public String getAnimalType() {
+    return animalType;
+  }
+}
