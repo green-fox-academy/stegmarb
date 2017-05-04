2cb6aa033cbb475cd2481c38626c1e7c62325450
Balazs Stegmar <stegmarb@gmail.com>
2017-05-04 12:14:39 +0200

    feat(helloworld): Autowire annotation was used and bean object has been created

--- Week-07/Day-04/HelloWorld/src/main/java/com/greenfoxacademy/HelloWorldConfig.java
+++ Week-07/Day-04/HelloWorld/src/main/java/com/greenfoxacademy/HelloWorldConfig.java
@@ -1,8 +1,13 @@
 package com.greenfoxacademy;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class HelloWorldConfig {
+  @Bean
+  public HelloWorld helloWorld() {
+    return new HelloWorld();
+  }
 
 }
