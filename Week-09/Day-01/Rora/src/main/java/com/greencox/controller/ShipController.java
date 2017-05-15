7c65e3455e2d927021dc72c36dac2c39c8f8290e
Balázs Stégmár <Balzs Stgmr>
2017-05-15 16:05:14 +0200

    init and feat(rora): ship filling method is linked to the given endpoint

--- Week-09/Day-01/Rora/src/main/java/com/greencox/controller/ShipController.java
+++ Week-09/Day-01/Rora/src/main/java/com/greencox/controller/ShipController.java
@@ -1,9 +1,12 @@
 package com.greencox.controller;
 
+import com.greencox.model.FilledShip;
 import com.greencox.model.Ship;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.annotation.RequestScope;
 
 @RestController
 public class ShipController {
@@ -11,4 +14,12 @@ public class ShipController {
   public Ship getShip() {
     return new Ship();
   }
+
+  @RequestMapping(value = "/rocket/fill", method = RequestMethod.GET)
+  public FilledShip filledShip(@RequestParam("caliber") String caliber, @RequestParam("amount") int amount) {
+    FilledShip filledShip = new FilledShip(caliber, amount);
+    filledShip.setStatusAndReadyness();
+    return filledShip;
+  }
 }
+
